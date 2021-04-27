ed23f2a3b78328df4d77197756ecdae80989bd85
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2021-04-26 18:28:40 -0600

    Update sbi_check_delegation.py
    sp -> hp

--- sbi_check_delegation.py
+++ sbi_check_delegation.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     
     last_cycle = conf_setup["last_cycle"]
     share_cycle_min = conf_setup["share_cycle_min"]
-    sp_share_ratio = conf_setup["sp_share_ratio"]
+    hp_share_ratio = conf_setup["hp_share_ratio"]
     rshares_per_cycle = conf_setup["rshares_per_cycle"]
     last_delegation_check = conf_setup["last_delegation_check"]
     
@@ -67,15 +67,15 @@ if __name__ == "__main__":
         # Update current node list from @fullnodeupdate
     
         delegation = {}
-        sum_sp = {}
-        sum_sp_shares = {}
-        sum_sp_leased = {}
+        sum_hp = {}
+        sum_hp_shares = {}
+        sum_hp_leased = {}
         account = "steembasicincome"
         delegation = {}
         delegation_shares = {}
-        sum_sp = 0
-        sum_sp_leased = 0
-        sum_sp_shares = 0
+        sum_hp = 0
+        sum_hp_leased = 0
+        sum_hp_shares = 0
         delegation_timestamp = {}
         
         print("load delegation")
@@ -126,7 +126,7 @@ if __name__ == "__main__":
             leased = transferStorage.find(acc, account)
             if len(leased) == 0:
                 delegation_shares[acc] = delegation_account[acc]
-                shares = int(delegation_account[acc] / sp_share_ratio)
+                shares = int(delegation_account[acc] / hp_share_ratio)
                 trxStorage.update_delegation_shares(account, acc, shares)
                 continue
             delegation_leased[acc] = delegation_account[acc]
@@ -137,14 +137,14 @@ if __name__ == "__main__":
         
         dd = delegation
         for d in dd:
-            sum_sp += dd[d]
+            sum_hp += dd[d]
         dd = delegation_leased
         for d in dd:
-            sum_sp_leased += dd[d]
+            sum_hp_leased += dd[d]
         dd = delegation_shares
         for d in dd:
-            sum_sp_shares += dd[d]                
-        print("%s: sum %.6f SP - shares %.6f SP - leased %.6f SP" % (account, sum_sp,  sum_sp_shares,  sum_sp_leased))
+            sum_hp_shares += dd[d]                
+        print("%s: sum %.6f HP - shares %.6f HP - leased %.6f HP" % (account, sum_hp,  sum_hp_shares,  sum_hp_leased))
     
     
         confStorage.update({"last_delegation_check": last_delegation_check})
